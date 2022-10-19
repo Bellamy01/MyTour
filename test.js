d065756afb7656f561dc4b057565bb9a7c7a3c64
Dan Bellamy <bdanbellamy@gmail.com>
2022-10-19 11:27:18 +0200

    auto-commit-8fc1bbf24720480a96970621d2ada6f9

--- test.js
+++ test.js
@@ -3,7 +3,10 @@ const fs = require('fs');
 
 const app = express();
 
+app.use(express.json());   
+
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`,"utf-8"))
+
 app.get('/api/v1/tours',(req,res)=>{
     res
     .status(200)
@@ -16,6 +19,20 @@ app.get('/api/v1/tours',(req,res)=>{
     });
 
 })
+
+app.post('/api/v1/tours',(req,res)=>{
+    const newId = tours[tours.length-1].id +1;
+    const newTour = Object.assign({id:newId},req.body);
+
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,tours,"utf-8",err=>{
+        if(err){
+            res.send(err.message);
+        }
+    })
+
+    res.send(newTour);
+});
 
 const port = 3000;
 app.listen(port,()=>{
