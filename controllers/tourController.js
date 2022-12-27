635644a65f002c302c3b86b68465a2549e44b9c1
Bellamy <bdanbellamy@gmail.com>
2022-12-27 09:16:23 +0200

    Added getAllTours controller

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -18,16 +18,23 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.getAllTours = (req, res) => {
-  //console.log(req.requestTime);
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    /* results: tours.length,
-    data: {
-      tours,
-    }, */
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      requestedAt: req.requestTime,
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 exports.getTour = (req, res) => {
   /* const tour = tours.find((el) => el.id == req.params.id);
