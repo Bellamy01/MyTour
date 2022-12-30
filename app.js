480cb05bc1c8a8d721c871d269608a2a1b2ad23a
Bellamy <bdanbellamy@gmail.com>
2022-12-30 23:12:10 +0200

    Data validation and Error handling(Debugging)

--- app.js
+++ app.js
@@ -16,12 +16,15 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  console.log('Request Sent Succesfully...');
-  next();
-});
 //3)ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
