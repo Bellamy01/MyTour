d688b6de0f5228e5dcb40f7016d89bfbf50cbf16
Bellamy <bdanbellamy@gmail.com>
2022-12-31 13:48:06 +0200

    Global Error Handling Middleware

--- app.js
+++ app.js
@@ -2,11 +2,16 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const app = express();
+//Utils and Controllers
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+
 //Routes
 
 const tourRouter = require('./routers/tourRoutes');
 const userRouter = require('./routers/userRoutes');
+
+const app = express();
 
 //1) MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
@@ -21,10 +26,13 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`,
-  });
+  /* const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404; */
+
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
+
+app.use(globalErrorHandler);
 
 module.exports = app;
