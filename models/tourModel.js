cec9ddedbdbaf5119c2bfced74bf82da491a36e4
Bellamy <bdanbellamy@gmail.com>
2023-04-24 07:57:25 +0200

    Embedding and child referencing tour guides

--- models/tourModel.js
+++ models/tourModel.js
@@ -2,7 +2,6 @@
 /* eslint-disable no-console */
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -102,7 +101,12 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
-    guides: Array,
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+      },
+    ],
   },
   {
     toJSON: { virtuals: true },
@@ -119,11 +123,18 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
-tourSchema.pre('save', async function (next) {
+tourSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'guides',
+    select: '-__v -passwordChangedAt',
+  });
+});
+
+/* tourSchema.pre('save', async function (next) {
   const guidesPromises = this.guides.map(async (id) => await User.findById(id));
   this.guides = await Promise.all(guidesPromises);
   next();
-});
+}); */
 
 /*
 
