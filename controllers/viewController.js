5382ce926309711ca2977c95180e5bd281d0287f
Bellamy <bdanbellamy@gmail.com>
2023-07-21 07:38:09 +0200

    Building the tour overview

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,16 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  //1) Get data
+  const tours = await Tour.find();
+  //2)Built template
+  //3)Render template
   res.status(200).render('overview', {
     title: 'All tours',
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
